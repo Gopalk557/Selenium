f35815a4f3279c982a8f9f11b4af80424d2ebf95
GOPAL THAKUR <gopalk557@gmail.com>
2022-05-31 23:26:26 +0530

    Practice problems wait

--- src/test/java/ImplicitAndExplicit/FluentWait.java
+++ src/test/java/ImplicitAndExplicit/FluentWait.java
@@ -3,30 +3,35 @@ package ImplicitAndExplicit;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
-public class ExplicitDemo
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class FluentWait
 {
-        @Test
-        public void ExplicitWait() throws InterruptedException
-{
+    @Test
+    public void FluentWait() throws InterruptedException
+    {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
+        //Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+           //     .withTimeout(Duration.ofSeconds(30))
+               // .pollingEvery(Duration.ofSeconds(5))
+              //  .ignoring(NoSuchElementException.class);
         driver.findElement(By.id("email")).sendKeys("gopaljithakur@gmail.com");
-        //Thread.sleep(2000);
-        //driver.findElement(By.id("email")).clear();
+        Thread.sleep(2000);
+        driver.findElement(By.id("email")).clear();
         Thread.sleep(2000);
         driver.findElement(By.name("pass")).sendKeys("GOPALK557@143");
-        new WebDriverWait(driver, Duration.ofSeconds(3)).
-                until(ExpectedConditions.elementToBeClickable(By.name("login")));
-        //driver.findElement(By.name("login")).click();
-}
+    }
 }
