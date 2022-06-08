179e2da8e22022b294868f5cfb7f50538d6c7a59
GOPAL THAKUR <gopalk557@gmail.com>
2022-06-09 03:10:34 +0530

    practice problems on list

---
+++ src/test/java/ListBox/SelectByIndex.java
@@ -0,0 +1,50 @@
+package ListBox;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class SelectByIndex
+{
+    @Test
+    public void SelectByIndexTest() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\CFP Automation\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Create New Account")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.name("firstname")).sendKeys("Gopal ji");
+        //Thread.sleep(2000);
+        //driver.findElement(By.name("lastname")).sendKeys("Thakur");
+        //Thread.sleep(2000);
+        driver.findElement(By.name("reg_email__")).sendKeys("gopalk@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.name("reg_email_confirmation__")).sendKeys("gopalk@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.name("reg_passwd__")).sendKeys("Gopal@143");
+        Thread.sleep(2000);
+
+        Select date = new Select(driver.findElement(By.id("day")));
+        date.selectByIndex(19);
+        Thread.sleep(2000);
+
+        Select month = new Select(driver.findElement(By.id("month")));
+        month.selectByIndex(9);
+        Thread.sleep(2000);
+
+        Select year = new Select(driver.findElement(By.id("year")));
+        year.selectByIndex(26);
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.name("websubmit")).click();
+        //driver.close();
+
+    }
+}
---
+++ src/test/java/ListBox/SelectByVisibleText.java
@@ -0,0 +1,51 @@
+package ListBox;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class SelectByVisibleText
+{
+    @Test
+    public void SelectByVisibleTextTest() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\CFP Automation\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Create New Account")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.name("firstname")).sendKeys("Gopal ji");
+        //Thread.sleep(2000);
+        driver.findElement(By.name("lastname")).sendKeys("Thakur");
+        Thread.sleep(2000);
+        driver.findElement(By.name("reg_email__")).sendKeys("gopalk@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.name("reg_email_confirmation__")).sendKeys("gopalk@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.name("reg_passwd__")).sendKeys("Gopal@143");
+        Thread.sleep(2000);
+
+        Select date1 = new Select(driver.findElement(By.id("day")));
+        date1.selectByVisibleText("20");
+
+        Thread.sleep(2000);
+
+        Select month = new Select(driver.findElement(By.id("month")));
+        month.selectByVisibleText("Oct");
+        Thread.sleep(2000);
+
+        Select year = new Select(driver.findElement(By.id("year")));
+        year.selectByVisibleText("1996");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.name("websubmit")).click();
+        //driver.close();
+
+    }
+}
